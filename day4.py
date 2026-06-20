3f7e3e9ba82d985f8ba5bc1b5fb84924488b487e
karan2342 <karanshrestha544@gmail.com>
2026-06-20 21:57:44 +0545

    Add user input handling and sorting functionality to day4.py

--- day4.py
+++ day4.py
@@ -6,3 +6,19 @@ n = [ 3, 1, 5, 19 , 45, 24, 34]
 
 print("Ascending :" ,sorted(n))
 print("Descending : " , sorted(n, reverse=True))
+
+
+numbers = []
+for i in range(10):
+    value = int(input(f"Enter numbers {i + 1}: " ))
+    numbers.append(value)
+
+
+ascending = sorted(numbers)
+descending = sorted(numbers, reverse=True)
+print("original list: ", numbers )
+print("Ascending order : ", ascending )
+print("Descending Order : ", descending)
+print("Largest number : ", ascending[-1])
+print("smallest number : " , ascending[0])
+print("Total sum : ", sum(numbers))
